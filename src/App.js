0165bf919004e397231692d330a8b65594a98a7d
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-02-05 23:11:21 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Home from './components/Home';
 import AddUser from './components/AddUser';
 import EditUser from './components/EditUser';
+import { GlobalProvider } from './context/GlobalState';
 
 //Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -11,11 +12,13 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   return (
     <div style={{maxWidth: '30rem', margin: '4rem auto'}}>
+    <GlobalProvider>
         <Routes>
             <Route path='/' element={<Home/>}/>
             <Route path='/add' element={<AddUser/>}/>
             <Route path='/edit/:id'  element={<EditUser/>}/>
         </Routes>
+    </GlobalProvider>
     </div>
   );
 }
